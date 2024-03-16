a0768a42a0077b99891369b45402e690bae1830e
Harshvardhan Patil <harshvardhanpatil@Harshvardhans-MacBook-Air.local>
2024-03-16 04:58:35 +0000

    Some more css enhancements

--- frontend/chat-client/src/app/chat/message.js
+++ frontend/chat-client/src/app/chat/message.js
@@ -4,7 +4,7 @@ const MessageComponent = ({ message, sender }) => {
       {sender === false ? (
         <div className="chat-message my-2">
           <div className="flex items-end">
-            <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
+            <div className="flex flex-col mx-2 order-2 items-start">
               <div>
                 <span className="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300 text-gray-600">
                   {message}
@@ -16,7 +16,7 @@ const MessageComponent = ({ message, sender }) => {
       ) : (
         <div className="chat-message my-2">
           <div className="flex items-end justify-end">
-            <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end">
+            <div className="flex flex-col mx-2 order-1 items-end">
               <div>
                 <span className="px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white ">
                   {message}
