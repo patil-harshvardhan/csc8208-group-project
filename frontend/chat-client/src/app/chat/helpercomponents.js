3731f9b7d542047ca52e42a04eb2a4e9ee0856dc
Harshvardhan Patil <harshvardhanpatil@Harshvardhans-MacBook-Air.local>
2024-03-19 07:33:15 +0000

    Online Offline status

---
+++ frontend/chat-client/src/app/chat/helpercomponents.js
@@ -0,0 +1,9 @@
+const OnlineDot = () => {
+  return <span class="flex w-3 h-3 me-3 bg-green-500 rounded-full"></span>;
+};
+const OfflineDot = () => {
+  return (
+    <span class="flex w-3 h-3 me-3 bg-gray-900 rounded-full dark:bg-gray-700"></span>
+  );
+};
+export { OnlineDot, OfflineDot };
--- frontend/chat-client/src/app/chat/leftpanel.js
+++ frontend/chat-client/src/app/chat/leftpanel.js
@@ -1,5 +1,6 @@
+import { OfflineDot, OnlineDot } from "./helpercomponents";
 
-const LeftPanel = ({users,setSelectedUser,userDetails,selectedUser}) => {
+const LeftPanel = ({users,setSelectedUser,userDetails,selectedUser,activeUsers}) => {
     return (
         <div className="bg-gray-200 h-full w-1/4 p-4">
                 <h1 className="text-lg font-bold mb-4">People</h1>
@@ -15,6 +16,7 @@ const LeftPanel = ({users,setSelectedUser,userDetails,selectedUser}) => {
                         alt="User"
                         className="w-8 h-8 rounded-full mr-2"
                       /> */}
+                      {activeUsers.map(u => u.id).includes(user.id) ? <OnlineDot/>:<OfflineDot/>}
                       <span className="font-semibold">
                         {user.email} {userDetails.id === user.id ? "(You)" : ""}
                       </span>
